1ead84b4deef4bbcf13de8398d42a76a1f580994
rkurr <rkurr@jvmguy.com>
2024-10-31 21:56:42 +0000

    Added Cucumber skeleton

---
+++ specification-by-example/src/test/java/org/kurron/hard/parts/payment/SpecificationByExampleIT.java
@@ -0,0 +1,11 @@
+package org.kurron.hard.parts.payment;
+
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+public class SpecificationByExampleIT {
+}
---
+++ specification-by-example/src/test/java/org/kurron/hard/parts/payment/Steps.java
@@ -0,0 +1,24 @@
+package org.kurron.hard.parts.payment;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Steps {
+
+    @Given("a running application")
+    public void a_running_application() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the health check is hit")
+    public void the_health_check_is_hit() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("a successful HTTP status code is returned")
+    public void a_successful_http_status_code_is_returned() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
